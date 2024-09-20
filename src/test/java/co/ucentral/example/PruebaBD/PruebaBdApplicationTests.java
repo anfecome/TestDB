3d58723d126fd92adf582f9ea723d2cd50fee01c
Your Name <you@example.com>
2024-09-19 19:23:16 -0500

    (New feature): Configuration Springboot

---
+++ src/test/java/co/ucentral/example/PruebaBD/PruebaBdApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.example.PruebaBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
